8959589e31af6b8a1a32e7e7a849b003956e6ab7
mawubaskyblair-cmyk <mawubaskyblair@gmail.com>
2026-09-01 23:26:49 +0300

    Add files via upload

---
+++ The_ ignore _code.php
@@ -0,0 +1,16 @@
+/*
+echo 'Welcome to my home!';
+echo 'Mi casa su casa!';
+*/
+echo 'Hello!';
+
+
+/*
+Rules for PHP variables:
+
+A variable must start with the $ sign, followed by the name of the variable
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive ($age and $AGE are two different variables)
+*/
